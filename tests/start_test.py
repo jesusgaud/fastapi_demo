11c24fd3375f3025e3492ea6c2746e1c3a15d36a
jesusgaud <jesusgaud@gmail.com>
2025-04-15 19:25:09 -0400

    Add updated CI/CD pipeline to build and push Docker image

--- tests/start_test.py
+++ tests/start_test.py
@@ -24,7 +24,7 @@ async def test_create_qr_code_unauthorized():
         "size": 10,
     }
     async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/qr-codes/", json=qr_request)
+        response = await ac.post("/qr_codes/", json=qr_request)
     assert response.status_code == 401  # Unauthorized
 
 @pytest.mark.asyncio
@@ -36,22 +36,28 @@ async def test_create_and_delete_qr_code():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         # Login and get the access token
         token_response = await ac.post("/token", data=form_data)
+        assert token_response.status_code == 200
         access_token = token_response.json()["access_token"]
         headers = {"Authorization": f"Bearer {access_token}"}
 
         # Create a QR code
         qr_request = {
-            "url": "https://example.com",
+            "url": "https://example.com/test-delete",
             "fill_color": "red",
             "back_color": "white",
             "size": 10,
         }
-        create_response = await ac.post("/qr-codes/", json=qr_request, headers=headers)
+        create_response = await ac.post("/qr_codes/", json=qr_request, headers=headers)
         assert create_response.status_code in [201, 409]  # Created or already exists
 
-        # If the QR code was created, attempt to delete it
+        # Try to extract filename from the 'links' or 'qr_code_url'
         if create_response.status_code == 201:
             qr_code_url = create_response.json()["qr_code_url"]
-            qr_filename = qr_code_url.split('/')[-1]
-            delete_response = await ac.delete(f"/qr-codes/{qr_filename}", headers=headers)
-            assert delete_response.status_code == 204  # No Content, successfully deleted
+        else:
+            qr_code_url = create_response.json()["links"][0]["href"]
+
+        qr_filename = qr_code_url.split("/")[-1]
+
+        # Delete QR Code
+        delete_response = await ac.delete(f"/qr_codes/{qr_filename}", headers=headers)
+        assert delete_response.status_code == 204  # No Content
